499210b04efa9aed456706ad6ecf603cb5eac1ad
Hai Victor Habi <haivictorh@mail.tau.ac.il>
2023-08-27 19:52:51 +0300

    Update metric_lister.py

--- pyresearchutils/metric_lister.py
+++ pyresearchutils/metric_lister.py
@@ -1,12 +1,15 @@
 import numpy as np
 from typing import Dict, List
-
+import pickle
 from pyresearchutils.logger import critical
 
 
 class MetricLister:
-    def __init__(self):
-        self.data: Dict[str, List] = dict()
+    def __init__(self, data=None):
+        if data is None:
+            self.data: Dict[str, List] = dict()
+        else:
+            self.data = data
 
     def add_value(self, **kwargs):
         for k, v in kwargs.items():
@@ -19,3 +22,20 @@ class MetricLister:
         if self.data.get(name) is None:
             critical(f"Can't find parameter{name}")
         return np.asarray(self.data[name])
+
+    def save2disk(self, location):
+        # Open a file and use dump()
+        with open(location, 'wb') as file:
+            # A new file will be created
+            pickle.dump(self.data, file)
+
+    @staticmethod
+    def load_data(location):
+        with open(location, 'rb') as file:
+            # A new file will be created
+            data = pickle.load(file)
+        return MetricLister(data)
+
+    def print_last(self):
+        for k, v in self.data.items():
+            print(k, "=", v[-1])
